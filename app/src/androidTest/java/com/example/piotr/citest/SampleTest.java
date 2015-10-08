5eaffef6f4ed4fc720e3fe9b50e50289e4c5afa4
Piotr Zmudzinski <ptr.zmudzinski@gmail.com>
2015-10-08 12:53:18 +0200

    added sample test

---
+++ app/src/androidTest/java/com/example/piotr/citest/SampleTest.java
@@ -0,0 +1,13 @@
+package com.example.piotr.citest;
+
+import android.test.AndroidTestCase;
+
+/**
+ * Created by piotr on 08/10/15.
+ */
+public class SampleTest extends AndroidTestCase {
+
+    public void testMySampleTest() {
+        assertEquals(true, true);
+    }
+}
